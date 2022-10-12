d37845c349498efe5b3b26f1f31a1a867c3d4f90
devShahin2022 <108648261+devShahin2022@users.noreply.github.com>
2022-10-12 22:23:22 +0600

    Update Banner.js

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -6,8 +6,8 @@ const Banner = () => {
     return (
         <div className='w-100 h-auto bg-info py-3'>
             <div className='container'>
-                <div className='d-flex pb-5'>
-                    <div className='w-50 py-5'>
+                <div className='res-flex d-flex pb-5'>
+                    <div className='col-b w-50 py-5'>
                         <h1 className='display-2'>Hello Learner</h1>
                         <p className='lead'>Don't be hesitate .Here you will justify your self that will be make you confident.Don't late click bottom and go to your kingdom</p>
                         <Button
@@ -18,7 +18,7 @@ const Banner = () => {
                             }}
                         className='py-2 px-4 mt-3' variant="primary">Let's get started</Button>
                     </div>
-                    <div className='w-50 pb-5'>
+                    <div className='col-b w-50 pb-5'>
                         <img id='logo' src={require('../../assets/img/banner.png')} alt=''></img>
                     </div>
                 </div>
